8ce68e2cbd4bfd5c269453d4a9af55d4720f7489
Gab <delossanto.adriangabriel@ue.edu.ph>
2020-10-04 19:04:43 +0800

    Added php code and file

--- accountPage.php
+++ accountPage.php
@@ -1,5 +1,7 @@
 <?php
 
+$userEmail =$_POST['emailAddress']
+$userName = $_POST['userName']
 $userPassword = $_POST['userPassword'];
 $userRepeatPassword = $_POST['userRepeatPassword'];
 
@@ -14,6 +16,24 @@ if($userPassword == $userRepeatPassword){
 	echo '<script>
 			window.history.go(-1);
 		 </script>';
+}
+
+function insertData($userName, $userPassword, $userEmail){
+	try{
+		require 'database.php'
+		
+		$sql =  "INSERT INTO accounts (userName, userPassword, mail) VALUES (?,?,?)";
+		
+		// no data returned
+		$conn->prepare($sql)->execute([$userName,$userPassword,$userEmail]);
+		
+		echo '<script>
+					alert ("Congrats")
+					</script>';
+	} catch (PDOException $e) {
+		echo $sql . "<br>" . $e->getMessage();
+	}
+	$conn = null;
 }
 
 
