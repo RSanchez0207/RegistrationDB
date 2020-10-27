d081e30f39c6e2d84c786f2b26a438cc74aeb557
delossantos.adriangabriel@ue.edu.ph <delossantos.adriangabriel@ue.edu.ph>
2020-10-27 13:17:41 +0800

    added login successfull alert

--- homePage.php
+++ homePage.php
@@ -19,7 +19,13 @@ $user = $check->fetch();
 
 if($user[0] == $loginUserName){
     //goto bionote
-    echo "bionote";
+    echo '<script>
+						alert("Login Succesfully");
+					 </script>';
+						
+						echo '<script>
+						window.history.go(-1);
+					 </script>';
 }
 else{
     echo '<script>
@@ -34,61 +40,6 @@ else{
 
 
 
-
-// //waiting for submit button
-// if($userPassword == $userRepeatPassword){
-// 	insertData($lastName,$firstName,$middleInitial
-// 	,$studentNumber,$yearLevel,$userBirthday
-// 	,$mobileNumber,$emailAddress,$userName
-// 	,$userPassword);
-// }else{
-// 	echo '<script>
-// 			alert("Password Mismatch");
-// 		 </script>';
-			
-// 	echo '<script>
-// 			window.history.go(-1);
-// 		 </script>';
-// }
-
-// function insertData($lastName,$firstName,$middleInitial,$studentNumber,$yearLevel,$userBirthday,$mobileNumber,$emailAddress,$userName,$userPassword){
-// 	try{
-		
-		
-// 		$sql =  "INSERT INTO usertable (lastName, firstName, middleInitial, studentNumber
-// 		, yearLevel, birthDate, mobileNumber, ueEmailAddress, userName, userPassword) VALUES (?,?,?,?,?,?,?,?,?,?)";
-		
-		
-// 		//username database
-// 		 $userNameDb = $conn->prepare("SELECT count(*) as cntUser FROM usertable WHERE username=:username");
-// 		   $userNameDb->bindValue(':username', $userName, PDO::PARAM_STR);
-// 		   $userNameDb->execute(); 
-// 		   $count = $userNameDb->fetchColumn();
-				
-// 				// check if username exist
-// 				if($count > 0){
-// 					echo '<script>
-// 						alert("Username Already Exist");
-// 					 </script>';
-						
-// 						echo '<script>
-// 						window.history.go(-1);
-// 					 </script>';
-// 				}else{
-// 					// insert of data to database
-// 					$conn->prepare($sql)->execute([$lastName,$firstName,$middleInitial
-// 						,$studentNumber,$yearLevel,$userBirthday
-// 						,$mobileNumber,$emailAddress,$userName
-// 						,$userPassword]);
-// 					sleep(2);
-// 					header("Location:HomePage.html");
-// 				}
-			
-// 	} catch (PDOException $e) {
-// 		echo $sql . "<br>" . $e->getMessage();
-// 	}
-// 	$conn = null;
-// }
 
 
 ?>
